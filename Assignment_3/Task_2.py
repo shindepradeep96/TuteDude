32b14042acfe0a2d611712eddf84b9da5c5f8059
shindepradeep96 <127085400+shindepradeep96@users.noreply.github.com>
2025-07-18 00:07:31 +0530

    Add files via upload
    Both Task has been completed successfully!

---
+++ Assignment_3/Task_2.py
@@ -0,0 +1,8 @@
+from math import *
+n = int(input("Enter a number: "))
+square_root = sqrt(n)
+logarithm = log(n)
+sine = sin(n)
+print("Square root:",square_root)
+print("Logarithm:",logarithm)
+print("Sine:",sine)
